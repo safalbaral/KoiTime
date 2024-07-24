3d91e3466fe9767c0b6501d3ed67000393a3226c
Safal Baral <130429214+safalbaral@users.noreply.github.com>
2024-07-24 13:32:18 +0545

    Added app icon and splash screen

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import TaskBar from "./TaskBar";
 import RecentTasksList from "./RecentTasksList";
 import tw from "twrnc";
@@ -8,8 +8,17 @@ import PrimaryTimer from "./PrimaryTimer";
 const Main = () => {
   return (
     <View style={[tw`flex-1`]}>
-      <View>
-        <Text style={tw`text-xl font-semibold text-center mt-4`}>AppTitle</Text>
+      <View
+        style={tw`flex flex-row gap-2 items-center bg-slate-50 self-center rounded-2xl px-4 pb-4 shadow-md shadow-blue-600`}
+      >
+        <Image
+          style={tw`w-8 h-8 mt-4`}
+          source={require("../../assets/icon.png")}
+          resizeMode="contain"
+        />
+        <Text style={tw`text-2xl text-center mt-4 text-slate-900 font-bold`}>
+          Koi Time
+        </Text>
       </View>
       <View style={tw`flex-1 rounded-t-full`}>
         <View style={tw`flex-1`}>
